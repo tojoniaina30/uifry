fcf533ed07fc2b278218e415649fb3040f31377a
tojoniaina <tojorar@gmail.com>
2023-01-24 09:45:17 +0300

    vita le doughnut

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 import './style/style.scss'
+import { Slider } from './style/js/sliding'
+import './style/js/half-daughnut'
+
+/* import { halfDaughnut } from './style/js/half-daughnut'
+
+
+var debut = new halfDaughnut ();
+debut.init() */
+
+var start = new Slider ( );
+start.init()
